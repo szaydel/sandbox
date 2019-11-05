8f10694227e04f0bcf2b148e64a5d4db7b32d9c3
Sam Zaydel <szaydel@gmail.com>
2019-11-05 14:37:32 +0000

    Clean-up style and fix off-by-one bug in avg_times

--- proc/procstat.c
+++ proc/procstat.c
@@ -24,17 +24,17 @@ static inline int time_since_boot(void) {
   procuptime = fopen("/proc/uptime", "r");
   fscanf(procuptime, "%d.%ds", &sec, &ssec);
   fclose(procuptime);
-  return (sec*sysconf(_SC_CLK_TCK))+ssec;
+  return (sec * sysconf(_SC_CLK_TCK)) + ssec;
 }
 
 // parse_times extracts CPU times from buf, which is expected to be
 // contents of /proc/<PID>/stat file.
 static inline times_t parse_times(char *buf) {
-  #define UTIME_IDX 13
-  #define STIME_IDX 14
-  #define CUTIME_IDX 15
-  #define CSTIME_IDX 16
-  #define STARTTIME_IDX 21
+#define UTIME_IDX 13
+#define STIME_IDX 14
+#define CUTIME_IDX 15
+#define CSTIME_IDX 16
+#define STARTTIME_IDX 21
 
   times_t t = {
       .user = 0,
@@ -43,20 +43,20 @@ static inline times_t parse_times(char *buf) {
   };
   char *tok;
 
-  for (size_t i = 0 ; (tok = strsep(&buf, " ")) != NULL ; i++) {
-      switch (i) {
-      case UTIME_IDX:
-      case CUTIME_IDX:
-        t.user += strtoul(tok, NULL, 10);
-        break;
-      case CSTIME_IDX:
-      case STIME_IDX:
-        t.sys += strtoul(tok, NULL, 10);
-        break;
-      case STARTTIME_IDX:
-        t.start = strtoul(tok, NULL, 10);
-        break;
-      }
+  for (size_t i = 0; (tok = strsep(&buf, " ")) != NULL; i++) {
+    switch (i) {
+    case UTIME_IDX:
+    case CUTIME_IDX:
+      t.user += strtoul(tok, NULL, 10);
+      break;
+    case CSTIME_IDX:
+    case STIME_IDX:
+      t.sys += strtoul(tok, NULL, 10);
+      break;
+    case STARTTIME_IDX:
+      t.start = strtoul(tok, NULL, 10);
+      break;
+    }
   }
   return t;
 }
@@ -84,29 +84,29 @@ static inline double cpu_ticks_per_tick(times_t first, times_t second) {
   cpu_times[1] = second.user + second.sys;
   times[0] = first.ts.tv_sec + ((double)first.ts.tv_nsec / 1e9);
   times[1] = second.ts.tv_sec + ((double)second.ts.tv_nsec / 1e9);
-  #ifdef DEBUG
+#ifdef DEBUG
   printf("cpu_times[0] = %f cpu_times[1] = %f\n", cpu_times[0], cpu_times[1]);
   printf("times[0] = %f times[1] = %f\n", times[0], times[1]);
-  #endif
+#endif
   return (cpu_times[1] - cpu_times[0]) /
-        ((times[1] - times[0]) * sysconf(_SC_CLK_TCK));
+         ((times[1] - times[0]) * sysconf(_SC_CLK_TCK));
 }
 
 // read_stat reads process stat information into a buffer for given PID.
 static inline char *read_stat(char *buf, size_t len, pid_t pid) {
-    FILE *input = NULL;
-    char *path = malloc(sizeof(char) * 1024);
-    if (!path) {
-      perror("malloc(...)");
-      return NULL;
-    }
-    asprintf(&path, "/proc/%d/stat", pid);
-    input = fopen(path, "r");
-    if(!input) {
-      free(path);
-      return NULL;
-    }
+  FILE *input = NULL;
+  char *path = malloc(sizeof(char) * 1024);
+  if (!path) {
+    perror("malloc(...)");
+    return NULL;
+  }
+  asprintf(&path, "/proc/%d/stat", pid);
+  input = fopen(path, "r");
+  if (!input) {
     free(path);
+    return NULL;
+  }
+  free(path);
 
   fread(buf, len, 1, input);
   if (ferror(input)) {
@@ -135,7 +135,7 @@ static inline times_t *collect_times(size_t n, pid_t pid) {
     free(buf);
     return NULL;
   }
-  for (size_t i = 0 ; i < n ; i++) {
+  for (size_t i = 0; i < n; i++) {
     if (read_stat(buf, bufsz, pid) == NULL) {
       free(tt);
       free(buf);
@@ -143,14 +143,14 @@ static inline times_t *collect_times(size_t n, pid_t pid) {
     }
     tt[i] = parse_times(buf);
     if (clock_gettime(CLOCK_MONOTONIC, &tt[i].ts) == -1) {
-        free(tt);
-        free(buf);
+      free(tt);
+      free(buf);
       return NULL;
     }
-    #ifdef DEBUG
-    printf("index[%zu] => %ld(sec) %ld(ns)\n",
-        i, tt[i].ts.tv_sec, tt[i].ts.tv_nsec);
-    #endif
+#ifdef DEBUG
+    printf("index[%zu] => %ld(sec) %ld(ns)\n", i, tt[i].ts.tv_sec,
+           tt[i].ts.tv_nsec);
+#endif
     sleep(1);
   }
   free(buf);
@@ -161,11 +161,11 @@ static inline times_t *collect_times(size_t n, pid_t pid) {
 // a diff between neighboring pairs, and then dividing that sum by
 // number of observations.
 static inline double avg_times(times_t *tt, size_t n) {
-    double total = 0;
-    for (size_t i = 0; i < n-1; i++) {
-        total += cpu_ticks_per_tick(tt[i], tt[i+1]);
-    }
-    return total / n;
+  double total = 0;
+  for (size_t i = 0; i < n - 1; i++) {
+    total += cpu_ticks_per_tick(tt[i], tt[i + 1]);
+  }
+  return total / (n - 1);
 }
 
 void usage(char *name) {
@@ -175,7 +175,7 @@ void usage(char *name) {
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-      usage(argv[0]);
+    usage(argv[0]);
   }
 
   if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
@@ -185,8 +185,8 @@ int main(int argc, char *argv[]) {
   pid_t pid = atoi(argv[1]);
 
   if (!pid) {
-      fprintf(stderr, "Failed to parse PID from argument\n");
-      return 1;
+    fprintf(stderr, "Failed to parse PID from argument\n");
+    return 1;
   }
   int intervals = 2;
   if (argc > 2) {
@@ -195,21 +195,19 @@ int main(int argc, char *argv[]) {
       if (parsed_intervals < 2) {
         parsed_intervals = 2;
       }
-    intervals = parsed_intervals;
+      intervals = parsed_intervals;
     }
   }
 
   times_t *points = collect_times(intervals, pid);
   if (points) {
-    printf("%f %f %f\n",
-        avg_times(points, intervals),
-        cpu_ticks_per_tick(points[0], points[intervals-1]),
-        lifetime_cpu_ticks_per_tick(points[intervals-1])
-    );
-  // pointless to free(points) here
+    printf("%f %f %f\n", avg_times(points, intervals),
+           cpu_ticks_per_tick(points[0], points[intervals - 1]),
+           lifetime_cpu_ticks_per_tick(points[intervals - 1]));
+    // pointless to free(points) here
   } else {
-      fprintf(stderr, "Failed to collect information, PID not valid?\n");
-      return 1;
+    fprintf(stderr, "Failed to collect information, PID not valid?\n");
+    return 1;
   }
   return 0;
 }
